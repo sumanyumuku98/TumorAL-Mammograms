569d01853d7e9da34b677bdfd4bd815fcd0a6fce
sumanyumuku98 <mukusumanyu@gmail.com>
2021-01-28 10:00:10 +0530

    AL script added

---
+++ AL.py
@@ -0,0 +1,268 @@
+import os
+import sys
+import torch
+import numpy as np
+import torchvision
+import torch.nn as nn
+from PIL import Image
+from detection.faster_rcnn import FasterRCNN
+from detection.faster_rcnn import FastRCNNPredictor, fasterrcnn_resnet50_fpn
+from dataset import AIIMS, get_transform, valAiimsAnn_path, valAiimsImg_path
+from misc import *
+from engine import train_one_epoch, evaluate
+import utils
+from torch.utils.data import DataLoader, Subset
+from torch.backends import cudnn
+from tqdm import tqdm
+import argparse
+import random
+from random import sample
+import json
+cudnn.benchmark=True
+
+parser = argparse.ArgumentParser(description="Arguments for Tumor dataset")
+parser.add_argument("--gpu", action='store_true')
+parser.add_argument("--bs", type=int, default=8)
+parser.add_argument("--epochs", type=int, default=1)
+parser.add_argument("--save_dir", type=str, default="./Tumor_iterations")
+parser.add_argument("--name", type=str, default="random")
+parser.add_argument("--budget", type=float, default=0.1)
+parser.add_argument("--AL", type=str, choices=["CS", "IR", "random", "MCD", "entropy"], default="random")
+parser.add_argument("--initial_ckpt", type=str, required=True)
+parser.add_argument("--initial_ids", type=str, required=True)
+parser.add_argument("--cycles", type=int, default=5)
+# parser.add_argument("--save_freq", type=int, default=1)
+parser.add_argument("--seed", type=int, default=23)
+parser.add_argument("--print_freq", type=int, default=20)
+
+args = parser.parse_args()
+
+random.seed(args.seed)
+np.random.seed(args.seed)
+torch.manual_seed(args.seed)
+torch.cuda.manual_seed(args.seed)
+torch.cuda.manual_seed_all(args.seed)
+
+if args.gpu or torch.cuda.is_available():
+    device="cuda"
+else:
+    device="cpu"
+
+save_dir = os.path.join(args.save_dir, args.name)
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+
+status_file = os.path.join(save_dir, "last_status.json")
+
+if not os.path.exists(status_file):
+    print("No init config found")
+    print("Initializing config...")
+    status_dict={}
+    status_dict["AL_iter"]=0
+    status_dict["last_checkpoint"] = args.initial_ckpt
+    status_dict["last_trainIds"] = args.initial_ids
+    with open(args.initial_ids, "r") as f:
+        initIds = f.readlines()
+
+    initIds=[int(x) for x in initIds]
+
+    complete_data = AIIMS(transform=get_transform(True))
+
+    full_length = len(complete_data)
+    sample_budget = int(args.budget * full_length)
+    stop_length = len(initIds) + args.cycles*sample_budget
+    status_dict["budget"] = sample_budget
+    status_dict["stop_len"] = stop_length
+    status_dict["AL"] = args.AL
+    status_dict["bs"] = args.bs
+    status_dict["epochs"] = args.epochs
+    status_dict["num_classes"] = len(complete_data.classes)
+
+    with open(status_file, "w") as f:
+        json.dump(status_dict, f, indent=2)
+
+
+with open(status_file, "r") as f:
+    init_status = json.load(f)
+
+print("<=======Current Config=======>")
+print(init_status)
+print("<============================>")
+checkpoint_file = init_status["last_checkpoint"]
+trainId_file = init_status["last_trainIds"]
+
+with open(trainId_file, "r") as f:
+    trainImageIds = f.readlines()
+trainImageIds = [int(x) for x in trainImageIds]
+
+sample_budget = int(init_status["budget"])
+stop_length = int(init_status["stop_len"])
+al_iteration = int(init_status["AL_iter"])
+algo_use = init_status["AL"]
+bs = int(init_status["bs"])
+num_classes = int(init_status["num_classes"])
+num_epochs = int(init_status["epochs"])
+
+
+print("Length of initial pool: %d" % len(trainImageIds))
+print("Sampling Budget: %d" % sample_budget)
+print("Training will stop when length of labelled pool is: %d" % stop_length)
+
+
+### Define function to train model for iteration
+"""
+Steps:
+    1) First sample data from unlabelled pool using specified_algo
+    2) Train model on selected pool
+"""
+
+def AL_iteration(checkpoint_file, train_ids_file, budget, stop_length, al_iteration, device="cpu"):
+
+    model = fasterrcnn_resnet50_fpn(pretrained=False)
+    in_features = model.roi_heads.box_predictor.cls_score.in_features
+    model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+    ckpt=checkpoint_file
+    print("Reading from checkpoint: %s" % ckpt)
+
+    state_dict = torch.load(ckpt)
+    model.load_state_dict(state_dict["model_state_dict"])
+    model.to(device)
+
+    with open(train_ids_file, "r") as f:
+        trainImageIds= f.readlines()
+
+    trainImageIds = [int(x) for x in trainImageIds]
+    unlabelData = AIIMS(transform=get_transform(False), trainIds=trainImageIds)
+
+    print("Length of unlabelled pool: %d" % len(unlabelData))
+
+    all_classes = unlabelData.classes
+    unlabelLoader = DataLoader(unlabelData, batch_size= 1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn)
+
+    if len(trainImageIds)==stop_length:
+        print("Cycles specified in Config has been completed!!")
+        sys.exit(0)
+    else:
+
+        if algo_use=="random":
+            selected_ids= sample(unlabelData.image_ids, budget)
+        elif algo_use=="IR":
+            ## get Ids using IR
+            selected_ids = irSet_Helper(model, unlabelLoader, budget, all_classes, save_dir, device)
+        elif algo_use=="CS":
+            ## get Ids using CS
+            selected_ids = coreSet_Helper(model, unlabelLoader, budget, all_classes, save_dir, device)
+        elif algo_use=="entropy":
+            ## Get ids using max entropy
+            selected_ids = maxEntropy_Helper(model, unlabelLoader, budget, all_classes, save_dir, device)
+        elif algo_use=="MCD":
+            ## GEt ids using MCD
+            selected_ids = MCD_Helper(model, unlabelLoader, budget, device)
+
+        del model, state_dict
+
+        print("Selected Pool of Length: %d" % len(selected_ids))
+
+        trainImageIds+=selected_ids
+
+        selection_file = os.path.join(save_dir, "%s_ALselection_iter_%d.txt" % (algo_use, al_iteration))
+
+        # with open(selection_file, "w") as f:
+        #     for item in selected_ids:
+        #         f.write("%d\n" % item)
+
+        train_file = os.path.join(save_dir, "trainImageIds_%d.txt" % al_iteration)
+
+        # with open(train_file, "w") as f:
+        #     for item in trainImageIds:
+        #         f.write("%d\n" % item)
+
+        init_status["last_trainIds"]=train_file
+
+        trainData=AIIMS(transform=get_transform(True), trainIds=selected_ids, mode="train")
+        valData=AIIMS(img_path=valAiimsImg_path, ann_path=valAiimsAnn_path, transform=get_transform(False))
+
+        print("Iteratively finetuning on data of length: %d" % len(trainData))
+        print("Will also validate on data of length %d" % len(valData))
+        trainLoader = DataLoader(trainData, batch_size=bs, shuffle=True, num_workers=6, collate_fn=utils.collate_fn)
+        valLoader = DataLoader(valData, batch_size=1, shuffle=False, num_workers=4, collate_fn=utils.collate_fn)
+
+
+        model = fasterrcnn_resnet50_fpn(pretrained=False)
+        in_features = model.roi_heads.box_predictor.cls_score.in_features
+        model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+        print("Loading weights from %s for training" % ckpt)
+
+        state_dict = torch.load(ckpt)
+        model.load_state_dict(state_dict["model_state_dict"])
+        model.to(device)
+
+        params = [p for p in model.parameters() if p.requires_grad]
+        optimizer = torch.optim.Adam(params, lr=1e-4)
+        lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                       step_size=3,
+                                                       gamma=0.5)
+
+        print("Training for %d epochs" % num_epochs)
+        global_val_loss = float("inf")
+
+        for epoch in tqdm(range(num_epochs)):
+            train_one_epoch(model, optimizer, trainLoader, device, epoch+1, print_freq=args.print_freq)
+            if torch.cuda.is_available():
+                torch.cuda.empty_cache()
+            lr_scheduler.step()
+
+            # Write validation part here if val loss is lower then only save model weights
+            print("Validating the current model..")
+            with torch.no_grad():
+                val_epoch_loss=0.0
+                
+                for images, targets in valLoader:
+                    images = list(image.to(device) for image in images)
+                    targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
+
+                    loss_dict = model(images, targets)
+
+                    losses = sum(loss for loss in loss_dict.values())
+                    loss_dict_reduced = utils.reduce_dict(loss_dict)
+                    losses_reduced = sum(loss for loss in loss_dict_reduced.values())
+
+                    loss_value = losses_reduced.item()
+                    val_epoch_loss+=loss_value
+
+                    del images, targets, loss_dict
+
+                print("Validation Loss for Epoch %d is %.3f" % (epoch+1, val_epoch_loss))
+
+                if val_epoch_loss<global_val_loss:
+                    global_val_loss=val_epoch_loss
+                    model_path = os.path.join(save_dir, "{}_ALmodel_iter_{}.pt".format(algo_use, al_iteration))
+                    torch.save({"epoch":epoch+1,
+                                "model_state_dict": model.state_dict(),
+                                "optimizer_state_dict":optimizer.state_dict()},
+                                model_path)
+                    print("Model saved at %s" % model_path)
+                    init_status["last_checkpoint"]=model_path
+            
+    print("Training in AL cycle: %d is completed" % al_iteration)
+
+    with open(selection_file, "w") as f:
+        for item in selected_ids:
+            f.write("%d\n" % item)
+
+    
+    with open(train_file, "w") as f:
+        for item in trainImageIds:
+            f.write("%d\n" % item)
+
+    # Save updated dict
+    print("Updating status dict...")
+    with open(status_file, "w") as f:
+        json.dump(init_status, f, indent=2)
+    
+
+if __name__=="__main__":
+    init_status["AL_iter"]=al_iteration+1
+    AL_iteration(checkpoint_file, trainId_file, sample_budget, stop_length, al_iteration+1, device)
---
+++ misc.py
@@ -0,0 +1,296 @@
+# Import libraries here.
+import numpy as np
+import torch
+import torch.nn as nn
+from scipy.special import softmax
+from scipy.stats import entropy
+from collections import defaultdict
+from random import sample
+from sklearn.metrics import pairwise_distances
+import utils
+from engine import evaluate
+from tqdm import tqdm
+
+
+
+
+
+def ir_numpy(array):
+    return np.std(array, axis=-1)/np.mean(array, axis=-1)
+
+
+def binary_cooccurence(cls_logits:np.ndarray):
+    # Assuming that background class has been removed
+    cooccur = np.zeros(cls_logits.shape[-1])
+    # probs = softmax(cls_logits, axis=-1)
+    # max_probs = np.max(probs, axis=-1)
+    # argmax_labels = np.argmax(probs, axis=-1)
+    labels = np.unique(np.argmax(cls_logits, axis=-1))
+    # assert max_probs.shape == argmax_labels.shape
+    
+    # ind = max_probs > thresh
+    # labels = np.unique(argmax_labels[ind])
+    cooccur[labels] = 1.0
+    return cooccur[1:]
+
+
+def k_center_modified(points_:np.ndarray, imageIds:list, dm, k:int):
+    """
+    Args:
+        points_: Unlabelled Pool
+        dm: distance measure in our case imbalance ratio
+        k: number of points to select
+    returns:
+        selected: Selected Pool of points
+    """
+    selected = []
+    selected_img_ids=[]
+    upool = list(zip(points_, imageIds))
+    ind1 = np.random.choice(len(upool))  # Choose first point randomly
+
+    p1 = upool[ind1]
+    selected.append(p1[0])                        # Add the first point to selected pool
+    selected_img_ids.append(p1[1])
+    # points_ = np.delete(points_, ind1, 0)      # Delete the point from unlabelled pool
+    del upool[ind1]
+    k-=1
+    while k!=0:
+        ir_list=[]             ### Maintain an i.r list for unlablled points.
+        for p2 in upool:             ## iterate in unlabeled pool
+            temp_list = selected.copy()           
+            temp_list.append(p2[0])
+            ir_list.append(dm(np.sum(temp_list, axis=0)))   # imbalance ratio of unlabelled point when it is added to the selected pool. 
+#         print(k, ir_list)
+        
+        min_ind = np.argmin(np.array(ir_list))  # Choose the point which has the minimum imbalance ratio.
+        p3 = upool[min_ind]                   
+        selected.append(p3[0])                     # Add that point to the selected pool
+        selected_img_ids.append(p3[1])
+        # points_ = np.delete(points_, min_ind, 0)
+        del upool[min_ind]
+        k-=1
+    return selected, selected_img_ids
+
+def maxEntropy(cls_logits:list, image_ids:list, budget:int):
+    entropy_list=[]
+    for arr in cls_logits:
+        probs = softmax(arr, axis=-1)
+        ent = entropy(probs, axis=-1)
+        entropy_list.append(ent.sum())
+    
+    ind = np.argsort(entropy_list)
+    reverse_ind = ind[::-1].tolist()
+#     print(reverse_ind)
+
+    sorted_rev_ids = [image_ids[idx] for idx in reverse_ind]
+    return sorted_rev_ids[:budget]
+
+
+class kCenterGreedy():
+    def __init__(self, X, metric='euclidean'):
+        self.features =X
+        self.metric = metric
+        self.min_distances = None
+        self.n_obs = X.shape[0]
+        self.already_selected = []
+    def update_distances(self, cluster_centers, only_new=True, reset_dist=False):
+
+        if reset_dist:
+            self.min_distances = None
+        if only_new:
+            cluster_centers = [d for d in cluster_centers
+                         if d not in self.already_selected]
+        if cluster_centers:
+            x = self.features[cluster_centers]
+            dist = pairwise_distances(self.features,x,metric='euclidean')
+            if self.min_distances is None:
+                self.min_distances = np.min(dist, axis=1).reshape(-1,1)
+            else:
+                self.min_distances = np.minimum(self.min_distances, dist)
+
+    def select_batch(self, already_selected, N):
+        try:
+            print('Calculating distances...')
+            self.update_distances(already_selected, only_new=False, reset_dist=True)
+        except:
+            print('Using flat_X as features.')
+            self.update_distances(already_selected, only_new=True, reset_dist=False)
+        new_batch = []
+        
+        for idx in range(N):
+            if self.already_selected is None or idx == 0:
+                ind = np.random.choice(np.arange(self.n_obs))
+            else:
+                ind = np.argmax(self.min_distances)
+            assert ind not in already_selected
+            self.update_distances([ind], only_new=True, reset_dist=False)
+
+            new_batch.append(ind)
+        print('Maximum distance from cluster centers is %0.2f'% max(self.min_distances))
+        self.already_selected = already_selected
+        return new_batch
+
+
+
+def coreset(points_:list, image_ids:list, budget:int):
+    """
+    points : image feature, if 100 images, 100x1000x12
+    image_id : image Ids
+    distance_fn: euclidean
+    budget
+    return : list of selected ids. 
+    """
+
+    points_ = np.stack(points_)
+    points_ = softmax(points_,axis=-1) # converting logits to softmax
+    a,b,c = points_.shape
+        
+    already_selected = []
+    points_ = np.reshape(points_,(a,b*c))
+    kc = kCenterGreedy(points_) # returns centers for the clusters
+    select = np.sort(kc.select_batch(already_selected,budget)) # select contains the index of selected features
+    
+    selection = []   
+    for s in select:
+        selection.append(image_ids[s]) # image ids for selected features
+    return selection
+
+
+def irSet_Helper(model, unlabelLoader, budget, all_classes, save_dir, device):
+    unlabel_ids, cls_logits, _ = evaluate(model, unlabelLoader, device, save_dir, all_classes, False)
+    cls_logits = [item.cpu().numpy() for item in cls_logits]
+    np_logits=[]
+    for arr in cls_logits:
+        ind = np.argmax(arr, axis=-1)!=0
+        new_arr= arr[ind]
+        arr = new_arr[:,1:]
+        np_logits.append(arr)
+    
+    weighted_list = [binary_cooccurence(item) for item in np_logits]
+    weighted_arr = np.stack(weighted_list)
+
+    if device=="cuda":
+        torch.cuda.empty_cache()
+
+    _, selected_ids = k_center_modified(weighted_arr, unlabel_ids, ir_numpy, budget)
+    return selected_ids
+
+# IRset based on model detetctions instead of logits
+
+def irSet_Helper2(model, unlabelLoader, budget, all_classes, save_dir, device, thresh=0.20):
+    unlabel_ids, _, all_detections = evaluate(model, unlabelLoader, device, save_dir, all_classes, False)
+    
+    labels = [item["labels"].numpy() for item in all_detections]
+    scores = [item["scores"].numpy() for item in all_detections]
+
+    final_labels=[]
+    for label_arr, score_arr in zip(labels, scores):
+        arr = np.zeros(len(all_classes),)
+        # print("Label arr initial shape:", label_arr.shape)
+        score_ind = score_arr>thresh
+        new_label = label_arr[score_ind]
+        # print("Label arr final shape:", label_arr.shape)
+
+        unique_labels = np.unique(new_label)
+        arr[unique_labels]=1.0
+        final_labels.append(arr[2:])
+    
+    # weighted_list = [binary_cooccurence(item) for item in np_logits]
+    weighted_arr = np.stack(final_labels)
+    # print(weighted_arr.shape)
+
+    if device=="cuda":
+        torch.cuda.empty_cache()
+
+    _, selected_ids = k_center_modified(weighted_arr, unlabel_ids, ir_numpy, budget)
+    return selected_ids
+
+
+
+def coreSet_Helper(model, unlabelLoader, budget, all_classes, save_dir, device):
+    unlabel_ids, cls_logits, _ = evaluate(model, unlabelLoader, device, save_dir, all_classes, False)
+    cls_logits = [item.cpu().numpy() for item in cls_logits]
+    ### This has been added as for some images the final proposed regions is not 1000 as usually is with resnet Backbone
+    processed_logits=[]
+    processed_ids=[]
+    for id_,arr in zip(unlabel_ids, cls_logits):
+        if arr.shape==(1000,len(all_classes)):
+            processed_logits.append(arr)
+            processed_ids.append(id_)
+
+    ###########################################
+    if device=="cuda":
+        torch.cuda.empty_cache()
+    
+    selected_ids = coreset(processed_logits, processed_ids, budget)
+    return selected_ids
+
+
+def maxEntropy_Helper(model, unlabelLoader, budget, all_classes, save_dir, device):
+    unlabel_ids, cls_logits, _ = evaluate(model, unlabelLoader, device, save_dir, all_classes, False)
+    cls_logits = [item.cpu().numpy() for item in cls_logits]
+    if device=="cuda":
+        torch.cuda.empty_cache()
+    
+    selected_ids = maxEntropy(cls_logits, unlabel_ids, budget)
+    return selected_ids
+
+
+def MCD(multi_logits_list:list, image_ids:list, budget:int):
+    """
+    multi_logits_list: each item in this list has the dimension (T*regions*classes). In MCD
+                       each input is forward passed to the model T times so we get T logits.
+    """
+    information_gain=[]
+    for arr in multi_logits_list:
+        probs = softmax(arr, axis=-1)
+        average_ = np.average(probs, axis=-1)
+        avg_entropy = entropy(average_, axis=-1).sum()
+
+        every_entropy = entropy(probs, axis=-1)
+        ent_average = np.average(every_entropy, axis=0).sum()
+
+        info_gain = avg_entropy - ent_average
+        information_gain.append(info_gain)
+
+    ind = np.argsort(information_gain)
+    reverse_ind = ind[::-1].tolist()
+    # print(reverse_ind)
+
+    sorted_rev_ids = [image_ids[idx] for idx in reverse_ind]
+    return sorted_rev_ids[:budget]
+
+
+
+def MCD_eval(m):
+    if type(m)== nn.Dropout:
+        m.train()
+    else:
+        m.eval() 
+
+
+def MCD_Helper(model, unlabelLoader, budget, device, T=5):
+    model.apply(MCD_eval)
+
+    model.to(device)
+    unlabel_ids=[]
+    multi_logits_list=[]
+    print("Evaluating images...")
+    for images, targets in tqdm(unlabelLoader):
+        images = list(img.to(device) for img in images)
+        unlabel_ids.append(targets[0]["image_id"].item())
+        temp_logits=[]
+        for i in range(T):
+            _, logit = model(images)
+            # print(logit.shape)
+            temp_logits.append(logit.detach().cpu().numpy())
+            del logit
+        
+        del images
+
+        final_logit = np.stack(temp_logits)
+        multi_logits_list.append(final_logit)
+
+    selection = MCD(multi_logits_list, unlabel_ids, budget)
+
+    return selection
